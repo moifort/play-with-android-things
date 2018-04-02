f6ed8c55b8786b1a2bdabb045750d95a9705bf35
tibo7n <Moifort91!>
2018-04-02 17:07:44 +0200

    Listener on capacitive buttons

--- app/src/main/java/me/mottet/things/MainActivity.kt
+++ app/src/main/java/me/mottet/things/MainActivity.kt
@@ -7,13 +7,17 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import com.google.android.things.contrib.driver.bmx280.Bmx280SensorDriver
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.GpioCallback
+import com.google.android.things.pio.PeripheralManager
 import com.google.firebase.database.FirebaseDatabase
 import java.util.*
 
 
-class MainActivity : Activity(), SensorEventListener {
+class MainActivity : Activity(), SensorEventListener, GpioCallback {
     private var count = 0
 
     private lateinit var sensor: Bmx280SensorDriver
@@ -43,6 +47,25 @@ class MainActivity : Activity(), SensorEventListener {
         sensor.registerTemperatureSensor()
         sensor.registerPressureSensor()
         sensor.registerHumiditySensor()
+
+
+        val buttonA = PeripheralManager.getInstance().openGpio("GPIO6_IO14")
+        buttonA.setDirection(Gpio.DIRECTION_IN)
+        buttonA.setEdgeTriggerType(Gpio.EDGE_BOTH)
+        buttonA.setActiveType(Gpio.ACTIVE_LOW)
+        buttonA.registerGpioCallback(this)
+
+        val buttonB = PeripheralManager.getInstance().openGpio("GPIO6_IO15")
+        buttonB.setDirection(Gpio.DIRECTION_IN)
+        buttonB.setEdgeTriggerType(Gpio.EDGE_BOTH)
+        buttonB.setActiveType(Gpio.ACTIVE_LOW)
+        buttonB.registerGpioCallback(this)
+
+        val buttonC = PeripheralManager.getInstance().openGpio("GPIO2_IO07")
+        buttonC.setDirection(Gpio.DIRECTION_IN)
+        buttonC.setEdgeTriggerType(Gpio.EDGE_BOTH)
+        buttonC.setActiveType(Gpio.ACTIVE_LOW)
+        buttonC.registerGpioCallback(this)
     }
 
     override fun onStart() {
@@ -57,6 +80,16 @@ class MainActivity : Activity(), SensorEventListener {
         sensorManager.registerListener(this, humdity, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
+    override fun onGpioEdge(gpio: Gpio): Boolean {
+        when (gpio.name) {
+            "GPIO6_IO14" -> database.reference.child("buttonA").setValue(Button(gpio.value))
+            "GPIO6_IO15" -> database.reference.child("buttonB").setValue(Button(gpio.value))
+            "GPIO2_IO07" -> database.reference.child("buttonC").setValue(Button(gpio.value))
+            else -> Log.e("TIBO", "pressure ${gpio.name}")
+        }
+        return true
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         sensor.unregisterHumiditySensor()
@@ -66,6 +99,7 @@ class MainActivity : Activity(), SensorEventListener {
     }
 
     override fun onAccuracyChanged(p0: Sensor, p1: Int) {
+
     }
 
     override fun onSensorChanged(event: SensorEvent) {
@@ -74,7 +108,7 @@ class MainActivity : Activity(), SensorEventListener {
         val value = event.values[0]
         when {
             event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE -> {
-                database.reference.child("weather").child("temperature").setValue(Temperature(value.toDouble(), System.currentTimeMillis()))
+                database.reference.child("sensor").child("temperature").setValue(Temperature(value.toDouble(), System.currentTimeMillis()))
             }
 //            event.sensor.type == Sensor.TYPE_PRESSURE -> Log.e("TIBO", "pressure $value")
 //            event.sensor.type == Sensor.TYPE_RELATIVE_HUMIDITY -> Log.e("TIBO", "humdity $value")
@@ -85,3 +119,4 @@ class MainActivity : Activity(), SensorEventListener {
 }
 
 data class Temperature(val value: Double, val timestamp: Long)
+data class Button(val state: Boolean)
